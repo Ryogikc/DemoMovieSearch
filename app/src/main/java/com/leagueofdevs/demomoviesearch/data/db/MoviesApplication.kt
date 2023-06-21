3f3a1a0309c2590c75d8dda33ed68ba258c0df8b
kcvarela <karina.cvarela.apps@gmail.com>
2023-06-21 17:59:31 -0600

    changes in workManager init

--- app/src/main/java/com/leagueofdevs/demomoviesearch/data/db/MoviesApplication.kt
+++ app/src/main/java/com/leagueofdevs/demomoviesearch/data/db/MoviesApplication.kt
@@ -2,13 +2,11 @@ package com.leagueofdevs.demomoviesearch.data.db
 
 import android.app.Application
 import androidx.work.Configuration
-import com.leagueofdevs.demomoviesearch.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MoviesApplication : Application(), Configuration.Provider {
-    override fun getWorkManagerConfiguration(): Configuration =
-        Configuration.Builder()
-            .setMinimumLoggingLevel(if (BuildConfig.DEBUG) android.util.Log.DEBUG else android.util.Log.ERROR)
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
             .build()
 }
--- app/src/main/java/com/leagueofdevs/demomoviesearch/data/db/MoviesDatabase.kt
+++ app/src/main/java/com/leagueofdevs/demomoviesearch/data/db/MoviesDatabase.kt
@@ -1,30 +1,12 @@
 package com.leagueofdevs.demomoviesearch.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.leagueofdevs.demomoviesearch.data.FavoriteMovieEntity
 import com.leagueofdevs.demomoviesearch.data.FavoriteMovieDao
+import com.leagueofdevs.demomoviesearch.data.FavoriteMovieEntity
 
 @Database(entities = [FavoriteMovieEntity::class], version = 1, exportSchema = false)
-abstract class MoviesDatabase : RoomDatabase(){
+abstract class MoviesDatabase : RoomDatabase() {
     abstract fun movieDao(): FavoriteMovieDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: MoviesDatabase? = null
-
-        fun getInstance(context: Context): MoviesDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    MoviesDatabase::class.java,
-                    "movies_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
